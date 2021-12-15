76ce39298a25b477b5ee0a2bce64596f5492f09b
LuisHenriqueSC1 <luishenrisc1@gmail.com>
2021-12-15 14:43:51 -0300

    insercao posts

--- wp-content/themes/pratica/index.php
+++ wp-content/themes/pratica/index.php
@@ -11,7 +11,30 @@
         <div class="container">
           <div class="row">
             <aside class="sidebar col-md-4">Barra Lateral</aside>
-            <div class="news col-md-8">Motícias</div>
+            <div class="news col-md-8">
+              <?php 
+              //Se houver algum post
+              if (have_posts()):
+                //Enquanto houver posts, mostre
+              while (have_posts()): the_post();
+              ?>
+              <article>
+                <h3><?php the_title(); ?></h3> <!-- mostra o titulo od post-->
+                            <!--Mostra a data de publicação-->   <!--Mostra o nome do autor do post-->
+                <p>Published in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
+                <!--Mostra a categoria do post--> <!--(' ') adiciona um espaço entre as categorias-->
+                <p>Categories: <?php the_category(' '); ?></p>
+                <!--Mostra o texto Tags antes de miostrar as tags presentes no pots-->
+                <p><?php the_tags('Tags: ', ','); ?></p>
+                <?php the_content(); ?>
+              </article>
+              <?php //Sera exibido caso não encontre nenhm post
+              endwhile;
+            else:
+              ?>
+              <p>There's nothing yet to be displayed...</p>
+               <?php endif; ?> <!--Encerramento do IF inicial -->
+            </div>
           </div>
         </div>
       </section>
